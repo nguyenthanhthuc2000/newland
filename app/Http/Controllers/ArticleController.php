dde5bf2e640de6dcd7165d77117aafe502812399
Nguyen Thanh Thuc <nguyenthanhthuc.2k@gmail.com>
2022-01-03 18:59:13 +0700

    add

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Repository\Article\ArticleRepositoryInterface;
+use App\Repository\User\UserRepositoryInterface;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    protected $userRepo;
+    protected $articleRepo;
+
+    public function __construct(
+        UserRepositoryInterface $userRepo,
+        ArticleRepositoryInterface $articleRepo
+    ){
+        $this->userRepo = $userRepo;
+        $this->articleRepo = $articleRepo;
+    }
 }
