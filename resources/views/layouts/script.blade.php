0d9028ae5ab6f27a0da65f24b1b37c2445627bba
thanhnhan0974 <64397349+thanhnhan0974@users.noreply.github.com>
2022-01-06 18:02:42 +0700

    add

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,8 +1,1 @@
-{{-- <script src="js/jquery-3.6.0.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/sweetalert2.min.js"></script>
---}}
-{{-- <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js"></script> --}}
-
 <script src="{{ asset('js/app.js') }}"></script>
