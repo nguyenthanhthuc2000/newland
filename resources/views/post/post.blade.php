159d65640437b01eefdefa6dd1f3f6eadfdb11f8
thanhnhan0974 <64397349+thanhnhan0974@users.noreply.github.com>
2022-01-06 16:08:38 +0700

    modify

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -22,7 +22,7 @@
                         <div class="dropdown">
                             <input placeholder="Tỉnh / Thành phố" class="form-control dropdown-toggle input-datalist" type="text" id="dropdownProvinces"
                                     data-bs-toggle="dropdown" aria-expanded="false" value="" autocomplete="off">
-                            <ul class="dropdown-menu" aria-labelledby="dropdownProvinces" datalist="dropdownProvinces" data-type="province">
+                            <ul class="dropdown-menu" aria-labelledby="dropdownProvinces" datalist="dropdownProvinces" data-type="provinces">
                                 @foreach ($province as $prov)
                                         <li value="{{ $prov->id }}">{{ $prov->_name }}</li>
                                 @endforeach
