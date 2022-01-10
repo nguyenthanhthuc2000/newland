f5d7ed1459fb74ecd519d5cb18e0436e3f731670
Nguyen Thanh Thuc <nguyenthanhthuc.2k@gmail.com>
2022-01-10 10:37:48 +0700

    add

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 @section('main')
     <div class="box-product pt-5 pb-5">
         <div class="row">
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6  box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
@@ -18,7 +18,23 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6  box__article mb-4">
+                <div class="card" >
+                    <a href="">
+                        <img src="{{asset('/uploads/article/12.jpg')}}" class="card-img-top" alt="...">
+                    </a>
+                    <a class="card-body card__article__body">
+                        <h5 class="text-split-2 card__title">Bán đất KDC Lộc Phát 2 - Ngay Trục (D30) kết nối 23/10 Với đại lộ Võ Nguyên Giáp - Sổ đỏ thổ cư.</h5>
+                        <p class="card-text mb-0 text-split-1 card__price"><strong>12 triệu/m² - 100 m²</strong></p>
+                        <p class="mb-0 text-split-1 card__address">Quận 3, Tp.Hồ Chí Minh</p>
+                        <div class="card__footer d-flex mt-3" style="justify-content: space-between; align-items: center">
+                            <p class="mb-0 card__time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
+                            <p class="mb-0"><i class="fal fa-heart card__heart"></i></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6  box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
@@ -34,7 +50,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6 box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
@@ -50,7 +66,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6 box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
@@ -66,23 +82,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 box__article mb-4">
-                <div class="card" >
-                    <a href="">
-                        <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
-                    </a>
-                    <a class="card-body card__article__body">
-                        <h5 class="text-split-2 card__title">Bán đất KDC Lộc Phát 2 - Ngay Trục (D30) kết nối 23/10 Với đại lộ Võ Nguyên Giáp - Sổ đỏ thổ cư.</h5>
-                        <p class="card-text mb-0 text-split-1 card__price"><strong>12 triệu/m² - 100 m²</strong></p>
-                        <p class="mb-0 text-split-1 card__address">Quận 3, Tp.Hồ Chí Minh</p>
-                        <div class="card__footer d-flex mt-3" style="justify-content: space-between; align-items: center">
-                            <p class="mb-0 card__time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
-                            <p class="mb-0"><i class="fal fa-heart card__heart"></i></p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6  box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
@@ -98,7 +98,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6 box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
@@ -114,7 +114,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 box__article mb-4">
+            <div class="col-lg-3 col-md-6 box__article mb-4">
                 <div class="card" >
                     <a href="">
                         <img src="{{asset('/uploads/article/123.jpg')}}" class="card-img-top" alt="...">
