97f11569ffd69906f0b55ad16aa8a6667616bb84
Nguyen Thanh Thuc <nguyenthanhthuc.2k@gmail.com>
2022-01-23 14:48:29 +0700

    css

--- resources/views/pages/category/index_category.blade.php
+++ resources/views/pages/category/index_category.blade.php
@@ -6,7 +6,7 @@
         <div class="l__a">
             <div class="l__a-article row mb-3">
                 <a href="" class="content-title-moblie ">
-                    <h5 class="text-split-2 mb-2"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp;Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
+                    <h5 class="text-split-2 mb-2">Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
                     </h5>
                 </a>
                 <a class="l__a-article-img col-md-4 col-6">
@@ -16,17 +16,18 @@
                     <a href="">
                         <h5 class="content-title text-split-2"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp; Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng</h5>
                         <p class="content-price mb-0 text-split-2"><strong><span> 12 triệu/m² - 100 m² &nbsp; </span> <span> Quận 12, Hồ Chí Minh</span></strong></p>
-                        <p class="content-desc text-split-3 mb-0">Some quick example text to build on the card title and make up the bulk. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="content-desc text-split-3 mb-0">She bul and make up the bulk of the card's content.</p>
                     </a>
                     <div class="content-footer d-flex" style="justify-content: space-between; align-items: center">
                         <p class="mb-0 card-time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
                         <p class="mb-0 card-heart"><i class="fal fa-heart"></i></p>
                     </div>
                 </div>
+                <span class="border-bt"></span>
             </div>
             <div class="l__a-article row mb-3">
                 <a href="" class="content-title-moblie ">
-                    <h5 class="text-split-2 mb-2"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp;Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
+                    <h5 class="text-split-2 mb-2">Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
                     </h5>
                 </a>
                 <a class="l__a-article-img col-md-4 col-6">
@@ -36,17 +37,18 @@
                     <a href="">
                         <h5 class="content-title text-split-2"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp; Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng</h5>
                         <p class="content-price mb-0 text-split-2"><strong><span> 12 triệu/m² - 100 m² &nbsp; </span> <span> Quận 12, Hồ Chí Minh</span></strong></p>
-                        <p class="content-desc text-split-3 mb-0">Some quick example text to build on the card title and make up the bulk. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="content-desc text-split-3 mb-0">She bul and make up the bulk of the card's content.</p>
                     </a>
                     <div class="content-footer d-flex" style="justify-content: space-between; align-items: center">
                         <p class="mb-0 card-time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
                         <p class="mb-0 card-heart"><i class="fal fa-heart"></i></p>
                     </div>
                 </div>
+                <span class="border-bt"></span>
             </div>
             <div class="l__a-article row mb-3">
                 <a href="" class="content-title-moblie ">
-                    <h5 class="text-split-2 mb-2"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp;Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
+                    <h5 class="text-split-2 mb-2">Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
                     </h5>
                 </a>
                 <a class="l__a-article-img col-md-4 col-6">
@@ -56,17 +58,18 @@
                     <a href="">
                         <h5 class="content-title text-split-2"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp; Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng</h5>
                         <p class="content-price mb-0 text-split-2"><strong><span> 12 triệu/m² - 100 m² &nbsp; </span> <span> Quận 12, Hồ Chí Minh</span></strong></p>
-                        <p class="content-desc text-split-3 mb-0">Some quick example text to build on the card title and make up the bulk. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="content-desc text-split-3 mb-0">She bul and make up the bulk of the card's content.</p>
                     </a>
                     <div class="content-footer d-flex" style="justify-content: space-between; align-items: center">
                         <p class="mb-0 card-time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
                         <p class="mb-0 card-heart"><i class="fal fa-heart"></i></p>
                     </div>
                 </div>
+                <span class="border-bt"></span>
             </div>
             <div class="l__a-article row mb-3">
                 <a href="" class="content-title-moblie ">
-                    <h5 class="text-split-2 mb-2"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp;Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
+                    <h5 class="text-split-2 mb-2">Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
                     </h5>
                 </a>
                 <a class="l__a-article-img col-md-4 col-6">
@@ -76,13 +79,56 @@
                     <a href="">
                         <h5 class="content-title text-split-2"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp; Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng</h5>
                         <p class="content-price mb-0 text-split-2"><strong><span> 12 triệu/m² - 100 m² &nbsp; </span> <span> Quận 12, Hồ Chí Minh</span></strong></p>
-                        <p class="content-desc text-split-3 mb-0">Some quick example text to build on the card title and make up the bulk. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="content-desc text-split-3 mb-0">She bul and make up the bulk of the card's content.</p>
                     </a>
                     <div class="content-footer d-flex" style="justify-content: space-between; align-items: center">
                         <p class="mb-0 card-time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
                         <p class="mb-0 card-heart"><i class="fal fa-heart"></i></p>
                     </div>
                 </div>
+                <span class="border-bt"></span>
+            </div>
+            <div class="l__a-article row mb-3">
+                <a href="" class="content-title-moblie ">
+                    <h5 class="text-split-2 mb-2">Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
+                    </h5>
+                </a>
+                <a class="l__a-article-img col-md-4 col-6">
+                    <img class="w-100 img-fluid img" src="{{asset('uploads/article/1.jpg')}}" alt="">
+                </a>
+                <div class="l__a-article-content col-md-8 col-6 p-3">
+                    <a href="">
+                        <h5 class="content-title text-split-2"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp; Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng</h5>
+                        <p class="content-price mb-0 text-split-2"><strong><span> 12 triệu/m² - 100 m² &nbsp; </span> <span> Quận 12, Hồ Chí Minh</span></strong></p>
+                        <p class="content-desc text-split-3 mb-0">She bul and make up the bulk of the card's content.</p>
+                    </a>
+                    <div class="content-footer d-flex" style="justify-content: space-between; align-items: center">
+                        <p class="mb-0 card-time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
+                        <p class="mb-0 card-heart"><i class="fal fa-heart"></i></p>
+                    </div>
+                </div>
+                <span class="border-bt"></span>
+            </div>
+            <div class="l__a-article row mb-3">
+                <a href="" class="content-title-moblie ">
+                    <h5 class="text-split-2 mb-2"><i class="fas fa-star"></i> Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng
+                    </h5>
+                </a>
+                <a class="l__a-article-img col-md-4 col-6">
+                    <img class="w-100 img-fluid img" src="{{asset('uploads/article/1.jpg')}}" alt="">
+                </a>
+                <div class="l__a-article-content col-md-8 col-6 p-3">
+                    <a href="">
+                        <h5 class="content-title text-split-2"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> &nbsp; Trực tiếp từ chủ đầu tư HANCO3 BRG ( không qua trung gian). Duy nhất 1 cơ hội cuối cùng a trung gian). Duy nhất 1 cơ hội cuối cùng</h5>
+                        <p class="content-price mb-0 text-split-2"><strong><span> 12 triệu/m² - 100 m² &nbsp; </span> <span> Quận 12, Hồ Chí Minh</span></strong></p>
+                        <p class="content-desc text-split-3 mb-0">She bul and make up the bulk of the card's content.</p>
+                    </a>
+                    <div class="content-footer d-flex" style="justify-content: space-between; align-items: center">
+                        <p class="mb-0 card-time"><i class="fal fa-calendar-alt"></i> 20/2/2022</p>
+                        <p class="mb-0 card-heart"><i class="fal fa-heart"></i></p>
+                    </div>
+                </div>
+                <span class="border-bt d-none"></span>
             </div>
         </div>
     </div>
