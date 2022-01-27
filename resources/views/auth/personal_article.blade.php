8ded3d281b9199e7740cd6629bfac3343f405814
thanhnhan0974 <64397349+thanhnhan0974@users.noreply.github.com>
2022-01-27 18:40:30 +0700

    modify

--- resources/views/auth/personal_article.blade.php
+++ resources/views/auth/personal_article.blade.php
@@ -6,16 +6,16 @@
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
             <button class="nav-link active" id="home-tab" data-bs-toggle="tab"
-            data-bs-target="#allArticle" type="button" role="tab" aria-controls="allArticle" aria-selected="true">Tất cả ({{ count($personalArticle) }})</button>
+            data-bs-target="#allArticle" type="button" role="tab" aria-controls="allArticle" aria-selected="true">Tất cả bài viết <strong>({{ $personalArticle->count() }})</strong></button>
         </li>
-        <li class="nav-item" role="presentation">
+        {{-- <li class="nav-item" role="presentation">
             <button class="nav-link" id="profile-tab" data-bs-toggle="tab"
             data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
         </li>
         <li class="nav-item" role="presentation">
             <button class="nav-link" id="contact-tab" data-bs-toggle="tab"
             data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
-        </li>
+        </li> --}}
     </ul>
     <div class="tab-content" id="personalArticle">
         <div class="tab-pane fade show active" id="allArticle" role="tabpanel" aria-labelledby="allArticle-tab">
