75c92166a49588c3954ee68ebcc54f43d14a91a4
Nguyen Thanh Thuc <nguyenthanhthuc.2k@gmail.com>
2022-01-27 11:48:37 +0700

    add

--- resources/views/admin/layouts/master_layout.blade.php
+++ resources/views/admin/layouts/master_layout.blade.php
@@ -47,33 +47,33 @@
                     <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/men/10.jpg" alt="">
                   </div>
                   <div class="peer">
-                    <span class="fsz-sm c-grey-900">John Doe</span>
+                    <span class="fsz-sm c-grey-900">{{ Auth::user()->name }}</span>
                   </div>
                 </a>
                 <ul class="dropdown-menu fsz-sm">
-                  <li>
-                    <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-                      <i class="ti-settings mR-10"></i>
-                      <span>Setting</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-                      <i class="ti-user mR-10"></i>
-                      <span>Profile</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="email.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-                      <i class="ti-email mR-10"></i>
-                      <span>Messages</span>
-                    </a>
-                  </li>
-                  <li role="separator" class="divider"></li>
+{{--                  <li>--}}
+{{--                    <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">--}}
+{{--                      <i class="ti-settings mR-10"></i>--}}
+{{--                      <span>Setting</span>--}}
+{{--                    </a>--}}
+{{--                  </li>--}}
+{{--                  <li>--}}
+{{--                    <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">--}}
+{{--                      <i class="ti-user mR-10"></i>--}}
+{{--                      <span>Profile</span>--}}
+{{--                    </a>--}}
+{{--                  </li>--}}
+{{--                  <li>--}}
+{{--                    <a href="email.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">--}}
+{{--                      <i class="ti-email mR-10"></i>--}}
+{{--                      <span>Messages</span>--}}
+{{--                    </a>--}}
+{{--                  </li>--}}
+{{--                  <li role="separator" class="divider"></li>--}}
                   <li>
                     <a href="{{ route('auth.get.logout') }}" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
                       <i class="ti-power-off mR-10"></i>
-                      <span>Logout</span>
+                      <span>Đăng xuất</span>
                     </a>
                   </li>
                 </ul>
@@ -91,7 +91,7 @@
 
         <!-- ### $App Screen Footer ### -->
         <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-          <span>Copyright © 2021 Designed by  </span>
+          <span>Copyright © 2021 {{$settings->name}}  </span>
         </footer>
       </div>
     </div>
